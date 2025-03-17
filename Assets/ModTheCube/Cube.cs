a7dc8bcab0195edefc7c0714c9364ce7134729aa
Gudboy <ninadnimkar2021@gmail.com>
2025-03-17 15:03:17 +0530

    Crazy Cube

---
+++ Assets/ModTheCube/Cube.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    public float rotation = 20;
+   
+    void Start()
+    {
+        transform.position = new Vector3(3, 4, 1);
+        
+        
+     
+    }
+    
+    void Update()
+    {
+        transform.Rotate(rotation * Time.deltaTime, rotation,rotation);
+
+        Material material = Renderer.material;
+        material.color = new Color(Random.value,Random.value, Random.value, Random.value);
+
+       int scale = Random.Range(1, 5);
+
+        transform.localScale = new Vector3(scale, scale, scale);    
+    }
+}
